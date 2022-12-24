edbf182de4a917480933703d1a12bc880feda7dd
leenaklaaib <lenaklaib237@gmail.com>
2022-12-24 19:00:02 +0300

    more of changes

--- frontend/post-job/post-job/postjop.js
+++ frontend/post-job/post-job/postjop.js
@@ -1,16 +1,23 @@
 const form =document.getElementById("post");
+const jopname =form.querySelector("#joptitle");
+const companyname=form.querySelector("#companyname");
 const Workplacetype = form.querySelector('[name=Workplace-type]');
+const joblocation =form.querySelector('#job-location');
 const jobtype = form.querySelector('[name=job-type]');
 
 Workplacetype.addEventListener('change', e => {
   const Workplace = e.target.value; 
-
+  
   jobtype.addEventListener('change', e => {
  const jobtype= e.target.value;
 
     fetch('', {
       method: 'POST',
-      body: JSON.stringify({ Workplace, jobtype })  
+      body: JSON.stringify({ 
+        title = jopname,
+
+
+       })  
     })
   });
 });
