71f8172c66d9f26046992e574313b4c74b6389ad
leenaklaaib <lenaklaib237@gmail.com>
2022-12-24 17:09:06 +0300

    lot

--- frontend/login file/login file/login.js
+++ frontend/login file/login file/login.js
@@ -1,17 +1,27 @@
-let usrename=document.querySelector('.input-field input');
-let password=document.querySelector('.input-field input');
-let getbutt=document.querySelector('.btn input');
+const form = document.getElementsByClassName("signinform");
+const btn = document.getElementsByClassName("btn");
+btn.addEventListener("click", handleFormSubmit(click));
 
-getbutt.addEventListener=function(){
-    checkfiled();
-};
 
-function checkfiled (){
-     if(usrename.value=="" || password.value==""){
-     alert("there is missing plase fill all filde")
-    }
-     
-
-}
+function handleFormSubmit(event) {
+    event.preventDefault(); 
+    
+    const formData = new FormData();
+    formData.append('username', document.getElementById('username').value);
+    formData.append('passowred', document.getElementById('passowred').value);
+  
+    
+    fetch('', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => {
+        
+    
+    })
+    .catch(error => {
+   
+    });
+  }
 
 
