ef33ffb689168c9229e74c0ccf72fa90e4808ced
leenaklaaib <lenaklaib237@gmail.com>
2022-12-24 22:15:29 +0300

    add file

--- frontend/post-job/post-job/postjop.js
+++ frontend/post-job/post-job/postjop.js
@@ -1,24 +1,34 @@
 const form =document.getElementById("post");
-const jopname =form.querySelector("#joptitle");
+const jobname =form.querySelector("#joptitle");
 const companyname=form.querySelector("#companyname");
 const Workplacetype = form.querySelector('[name=Workplace-type]');
 const joblocation =form.querySelector('#job-location');
-const jobtype = form.querySelector('[name=job-type]');
-
+const jobtypes = form.querySelector('[name=job-type]');
+let workplace ;
+let jobtype;
+document.getElementsByName(button).addEventListener('click', setattibut());
 Workplacetype.addEventListener('change', e => {
-  const Workplace = e.target.value; 
+   Workplace = e.target.value; })
   
-  jobtype.addEventListener('change', e => {
- const jobtype= e.target.value;
+  jobtypes.addEventListener('change', e => {
+  jobtype= e.target.value;})  
+ function setattibut(){
+jobname=jobname.value;
+companyname=companyname.value;
+joblocation =joblocation.value;
+fetch('', {
+  method: 'POST',
+  body: JSON.stringify({ 
+    title : jobname,
+    companyname:companyname,
+    location:joblocation,
+    Workplace:Workplacetype,
+    jobtype:jobtype })  
+})
 
-    fetch('', {
-      method: 'POST',
-      body: JSON.stringify({ 
-        title = jopname,
+
+ }
 
 
-       })  
-    })
-  });
-});
+  
 
