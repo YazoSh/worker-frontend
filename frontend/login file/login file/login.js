4ebc620cd7a883c01439f7152391c6a9708da6f6
leenaklaaib <lenaklaib237@gmail.com>
2022-12-25 16:25:01 +0300

    some edit

--- frontend/login file/login file/login.js
+++ frontend/login file/login file/login.js
@@ -4,24 +4,25 @@ btn.addEventListener("click", handleFormSubmit(click));
 
 
 function handleFormSubmit(event) {
-    event.preventDefault(); 
-    
+    event.preventDefault();
     const formData = new FormData();
     formData.append('username', document.getElementById('username').value);
     formData.append('passowred', document.getElementById('passowred').value);
-  
+    cheack();
+}
     
-    fetch('', {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => {
-        
-    
-    })
-    .catch(error => {
-   
-    });
-  }
+   function cheack (formData)(
+    fetch('')
+     .then(response => response.json())
+     .then(users =>{
+  for( var i=0 ; i < users.length ; i++){
+    if (users[i].name === document.getElementById('username').value)
+    if( users[i].passoerd === document.getElementById('passowred').value)
+     alert("log in sucsseful");
+  }}).catch(e=>{
+   alert("cheack your passowerd and username");})
 
 
+   )
+
+ 
